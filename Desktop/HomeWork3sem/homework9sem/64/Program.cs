412195d8f775f96255f3c1623ef9fc87eb188837
alex <gerotyk9999@gmail.com>
2023-02-20 17:21:29 +0300

    64 готово

---
+++ Desktop/HomeWork3sem/homework9sem/64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine(
+    $"Задача 64:\nЗадайте значение N. Напишите программу, которая выведет все натуральные числа\n" +
+    $"в промежутке от N до 1. Выполнить с помощью рекурсии.\n"
+);
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Console.Clear();
+
+
+numbersAtN2One(N);
+
+void numbersAtN2One(int start, int end = 1){
+    Console.Write($"{start} ");
+    if(start == end){return;};
+    numbersAtN2One(start - 1, end);
+};
